e56f64f84c5fe9e171b87f0805cb8ace25fa9c6d
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-16 23:35:39 -0500

    Creación de usuario y contraseña

--- Preinforme11_SantiagoDelValle.py
+++ Preinforme11_SantiagoDelValle.py
@@ -1,28 +1,27 @@
 import numpy as np
 
 #Tema de ciberseguridad
-
-print("""Bienvenido a la base de datos
-Pd: esta es una versión alpha""")
-
-x=int(input("""Por favor digite su contraseña para acceder,sir Carlos
-:"""))
+#Creación de usuario
 
 contraseña=[[0,1,2,3,4,8,9],[10,54,39,74,75,36,14],[1000,3000,3100,5100,4872,4789,36521],[100008,1411515,18556,23864,48562,19845,32389]]
 
-contraseña_1= contraseña [0][5]
-contraseña_2= contraseña [2][6]
-contraseña_3= contraseña [3][6]
-contraseña_4= contraseña [1][0]
+User_Name= input("Por favor digite su nombre de usuario")
 
-contraseña_usuario= contraseña_1 + contraseña_2 + contraseña_3 + contraseña_4
+n= int(input("Digite un numero entre 1 y 8"))
+z= int(input("Digite un numero entre 1 y 8"))
+x= int(input("Digite un numero entre 1 y 8"))
+k= int(input("Digite un numero entre 1 y 8"))
+p= int(input("Digite un numero entre 1 y 8"))
+o= int(input("Digite un numero entre 1 y 8"))
 
-if x==contraseña_usuario:
-    print("Puede acceder")
+if 0<=n<=8 and 0<=z<=8 and 0<=x<=8 and 0<=k<=8 and 0<=p<=8 and 0<=o<=8:
+
+    parte_1= contraseña[n-1][z-1]
+    parte_2= contraseña[x-1][k-1]
+    parte_3= contraseña[p-1][o-1]
+
+    contraseña_Final= parte_1 * parte_2 * parte_3
+
+    print("Su contraseña es",contraseña_Final)
 else:
-    print("Intentelo otra vez")
-            
-        
-    
-        
-
+    print("Por favor haga bien el procedimiento")
